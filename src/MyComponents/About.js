22f40c9d1e7bccf41e9f33262fb108d1a1589575
ved24sharma <86173210+ved24sharma@users.noreply.github.com>
2021-07-05 21:01:10 -0400

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            it is about component
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus debitis minus labore esse praesentium accusamus, id repellat ratione beatae repudiandae et, culpa, nemo deserunt! Inventore ab distinctio dolorum necessitatibus beatae.</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+
+
+    return (
+        <footer className="bg-dark text-light">
+            <p className="text-center py-3">
+                Copyright &copy; My Todos List.com
+            </p>
+        </footer>
+    )
+}
